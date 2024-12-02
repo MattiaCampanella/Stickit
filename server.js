abda89dd3a91caba421eb97a335e88182b4a2c80
Mattia Campanella <jzmatx6311@gmail.com>
2024-12-02 14:00:08 +0100

    added database for users -NEEDS TESTS-

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const http = require("http");
 const bcrypt = require("bcryptjs");
-const { randomInt } = require("crypto");
 const bodyParser = require("body-parser");
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://C0MPLX:iVUok9MWXTRCZlSi@testcluster.wi4rn.mongodb.net/?retryWrites=true&w=majority&appName=testCluster";
 
 const app = express();
 const server = http.createServer(app);
@@ -16,26 +18,57 @@ server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
+//database connection
+const client = new MongoClient(uri, { 
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+});
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 //test user//
 const user = { username: "admin", password: "admin" };
 
-app.post("/login", (req, res)=>{
+app.post("/board", (req, res)=>{
     const {username, password} = req.body;
 
-    //check credentials - subsitute here
-    if (username !== user.username || password !== user.password) {
-        return res.status(400).json({error:"Wrong Credentials"});
+    if(username === user.username && password === user.password){ //check if user is admin
+        res.sendFile(__dirname + "/www/blankCanvas.html");
     }
-    //subsitute here
 
+    //check users credentials in db.users
+    if(db.users.findOne({username: username}) === null){
+        return res.status(400).json({error:"User not found"});
+    }
+    if(!bcrypt.compareSync(password, db.users.findOne({username: username}).password)){
+        return res.status(400).json({error:"Wrong password"});
+    }
     res.sendFile(__dirname + "/www/blankCanvas.html");
 });
 
 app.post("/register", (req, res)=>{
     const {username, password} = req.body;
 
-    //insert user into db
+    //insert user into db.users
+    db.users.insertOne({
+        username: username,
+        password: bcrypt.hashSync(password, 8)
+    });
 
     res.json({success:true});
 });
